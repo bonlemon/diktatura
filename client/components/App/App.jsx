143fd83429293b6ffa844c80b378a79aaf3e51dc
bonlemon <dimon009@list.ru>
2018-03-28 23:56:07 +0300

    change footer

--- client/components/App/App.jsx
+++ client/components/App/App.jsx
@@ -41,17 +41,17 @@ class App extends React.Component {
                     <AppBar>
                         <div className="container">
                             <div className="row">
-                                <div className="col-4">
+                                <div className="col-4 d-none d-md-block">
                                     <div className="header__contact">
                                         +7 (999) 717 09 71
                                     </div>
                                 </div>
-                                <div className="col">
+                                <div className="col-md-6">
                                     <div className="header__logo">
                                         <img src={logo} alt="Logo" />
                                     </div>
                                 </div>
-                                <div className="col">
+                                <div className="col d-none d-md-block">
                                     <div className="header__icons">
                                         <i class="fa fa-user fa-lg"></i>
                                     </div>
@@ -85,15 +85,22 @@ class App extends React.Component {
                         <div className="row">
                             <div className="col">
                                 <div className="footer__links">
-                                    <form className="footer__subscribe">
-                                        <input type="text"/>
-                                        <button>Подписаться</button>
-                                    </form>
-                                    <div className="footer_social">
-                                        <i className="fa fa-facebook-f fa-lg footer__social-img"></i>
-                                        <i className="fa fa-vk fa-lg footer__social-img"></i>
-                                        <i className="fa fa-instagram fa-lg footer__social-img"></i>
-                                        <i className="fa fa-telegram-plane fa-lg footer__social-img"></i>
+                                    <div className="row">
+                                        <div className="col">
+                                            <form className="footer__subscribe">
+                                                <input type="text"/>
+                                                <button>Подписаться</button>
+                                            </form>
+                                        </div>
+                                        <div className="w-100 d-block d-md-none"></div>
+                                        <div className="col">
+                                            <div className="footer_social">
+                                                <i className="fa fa-facebook-f fa-lg footer__social-img"></i>
+                                                <i className="fa fa-vk fa-lg footer__social-img"></i>
+                                                <i className="fa fa-instagram fa-lg footer__social-img"></i>
+                                                <i className="fa fa-telegram-plane fa-lg footer__social-img"></i>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
